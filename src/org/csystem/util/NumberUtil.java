863d4199048cfc4e75ca5347345741e12eba1e65
gencdevops <67705077+gencdevops@users.noreply.github.com>
2020-12-29 11:53:52 +0300

    getFibonacciNumber method was added

--- src/org/csystem/util/NumberUtil.java
+++ src/org/csystem/util/NumberUtil.java
@@ -48,6 +48,26 @@ public class NumberUtil {
         return sum;
     }
 
+    public static int getFibonacciNumber(int n)
+    {
+        if (n <= 0)
+            return -1;
+
+        if (n <= 2)
+            return n - 1;
+
+        int prev1 = 1, prev2 = 0, val = 0;
+
+        for (int i = 2; i < n; ++i) {
+            val = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = val;
+        }
+
+        return val;
+    }
+
+
 
     public static int getNextFibonacciNumber(int val)
     {
