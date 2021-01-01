1bee29ed4a9fe4886d9fcdc23adb4754d53051ff
gencdevops <67705077+gencdevops@users.noreply.github.com>
2021-01-01 13:30:17 +0300

    GetDayOfWeekTest initialize

--- src/org/csystem/app/App.java
+++ src/org/csystem/app/App.java
@@ -2,12 +2,10 @@ package org.csystem.app;
 class App {
 
     public static void main(String [] args) {
-    String [] names = {"taner" , "murat" , "ali" , "mahmut"};
-    for(int i = 0; i < names.length; i++)
-        System.out.println(names[i].toUpperCase());
 
-    for(int i = 0; i < names.length; i++)
-        System.out.println(names[i]);
+
+
+
 
     }
 }
