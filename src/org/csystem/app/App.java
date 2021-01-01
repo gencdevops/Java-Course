402b29afb68aa76ea413f9ee1253229b396ca243
gencdevops <67705077+gencdevops@users.noreply.github.com>
2021-01-01 13:08:44 +0300

    DateUtil initialize

--- src/org/csystem/app/App.java
+++ src/org/csystem/app/App.java
@@ -1,11 +1,14 @@
 package org.csystem.app;
 class App {
 
-    public static void main(String [] args)
-    {
+    public static void main(String [] args) {
+    String [] names = {"taner" , "murat" , "ali" , "mahmut"};
+    for(int i = 0; i < names.length; i++)
+        System.out.println(names[i].toUpperCase());
 
-
-
+    for(int i = 0; i < names.length; i++)
+        System.out.println(names[i]);
 
     }
 }
+
