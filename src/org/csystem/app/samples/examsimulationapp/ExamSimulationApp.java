29f9438cdf832cddc98611cb6fa0131cd5936b6c
gencdevops <67705077+gencdevops@users.noreply.github.com>
2021-01-07 16:42:59 +0300

    ExamSimulationApp.run() was added

--- src/org/csystem/app/samples/examsimulationapp/ExamSimulationApp.java
+++ src/org/csystem/app/samples/examsimulationapp/ExamSimulationApp.java
@@ -1,5 +1,15 @@
 package org.csystem.app.samples.examsimulationapp;
 
 public class ExamSimulationApp {
+    public static void run()
+    {
+        ExamSimulation physicSimulation = new ExamSimulation("Fizik");
+        ExamSimulation mathSimulation = new ExamSimulation("Matematik");
 
+        physicSimulation.run();
+        mathSimulation.run();
+        physicSimulation.displayResults();
+        System.out.println("//////////////////////////////////////////////////////////");
+        mathSimulation.displayResults();
+    }
 }
