59eabced74ea9f08beb684b3cd201d3fe5097a88
gencdevops <67705077+gencdevops@users.noreply.github.com>
2021-01-07 16:39:37 +0300

    ExamSimulation.fillGrades() was added

--- src/org/csystem/app/samples/examsimulationapp/ExamSimulation.java
+++ src/org/csystem/app/samples/examsimulationapp/ExamSimulation.java
@@ -1,11 +1,30 @@
 package org.csystem.app.samples.examsimulationapp;
 
+import org.csystem.util.ArrayUtil;
+
+import java.util.Random;
+import java.util.Scanner;
+
 public class ExamSimulation {
     public String lectureName;
     public int [][] grades;
     public double [] averages;
     public double average;
 
+    public void fillGrades()
+    {
+        Random r = new Random();
+        Scanner kb = new Scanner(System.in);
+        System.out.printf("%s sınavına girecek şube sayısını giriniz:", lectureName);
+        grades = new int[Integer.parseInt(kb.nextLine())][];
+        averages = new double[grades.length];
 
+        for (int i = 0; i < grades.length; ++i) {
+            System.out.printf("%d. şube öğrenci sayısını giriniz:", i + 1);
+            int count = Integer.parseInt(kb.nextLine());
+
+            grades[i] = ArrayUtil.getRandomArray(r, count, 0, 101);
+        }
+    }
 
 }
