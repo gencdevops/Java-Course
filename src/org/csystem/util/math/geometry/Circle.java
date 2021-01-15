5c975b3a55c4eebd48cd35a6f01d3f518194f80a
gencdevops <67705077+gencdevops@users.noreply.github.com>
2021-01-15 21:53:01 +0300

    geometry.Circle getRadius() getArea() getCircumference() was added

--- src/org/csystem/util/math/geometry/Circle.java
+++ src/org/csystem/util/math/geometry/Circle.java
@@ -5,7 +5,17 @@ private double m_r;
 private double m_area;
 private double m_circumference;
 
+public double getRadius() {
+    return m_r;
+}
 
+public double getArea() {
+    return m_area;
+}
+
+public double getCircumference() {
+    return m_circumference;
+}
 
 
 
