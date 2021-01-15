a38897e553c8f5033b50860e0ff104acb5f4f36f
gencdevops <67705077+gencdevops@users.noreply.github.com>
2021-01-15 22:16:02 +0300

    geometry.Circle constructors has been defined

--- src/org/csystem/app/App.java
+++ src/org/csystem/app/App.java
@@ -1,11 +1,13 @@
 package org.csystem.app;
 
+import org.csystem.util.math.geometry.Circle;
+
 class App {
 
     public static void main(String [] args) {
 
-       String s = "1234596";
-       int m = Integer.parseInt(s.substring(1 , 5));
+        Circle c = new Circle();
+        System.out.println(c.getArea());
 
 
 
--- src/org/csystem/util/math/geometry/Circle.java
+++ src/org/csystem/util/math/geometry/Circle.java
@@ -5,8 +5,25 @@ private double m_r;
 private double m_area;
 private double m_circumference;
 
+public Circle() {
+
+}
+public Circle(double r) {
+    setRadius(r);
+}
+
+private void calculate() {
+    m_area = Math.PI * m_r * m_r;
+    m_circumference = 2 * Math.PI * m_r;
+}
+
 public double getRadius() {
     return m_r;
+}
+
+public void setRadius(double r) {
+    m_r = Math.abs(r);
+    calculate();
 }
 
 public double getArea() {
