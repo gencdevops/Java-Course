1423bbb97ce78a05b0b12520290388e2a38ac22f
gencdevops <67705077+gencdevops@users.noreply.github.com>
2021-01-21 19:40:46 +0300

    IntValue subcract , compareTo, multiply, add,  methods were added

--- src/org/csystem/util/IntValue.java
+++ src/org/csystem/util/IntValue.java
@@ -1,3 +1,6 @@
+/*----------------------------------------------------------------------------------------------------------------------
+    IntValue sınıfı
+----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.util;
 
 public class IntValue {
@@ -13,7 +16,6 @@ public class IntValue {
     public static final IntValue ONE = of(1);
     public static final IntValue TEN = of(10);
 
-
     public static IntValue of(int val)
     {
         if (val < -128  || val > 127)
@@ -25,5 +27,59 @@ public class IntValue {
         return CACHE[val + 128];
     }
 
+    public int getVal()
+    {
+        return m_val;
+    }
 
+    public IntValue add(int val)
+    {
+        return IntValue.of(m_val + val);
+    }
+
+    public IntValue add(IntValue intValue)
+    {
+        return add(intValue.m_val);
+    }
+
+    public IntValue multiply(int val)
+    {
+        return IntValue.of(m_val * val);
+    }
+
+    public IntValue multiply(IntValue intValue)
+    {
+        return multiply(intValue.m_val);
+    }
+
+    public IntValue subtract(int val)
+    {
+        return add(-val);
+    }
+
+    public IntValue subtract(IntValue intValue)
+    {
+        return subtract(intValue.m_val);
+    }
+
+    public int compareTo(IntValue other)
+    {
+        return m_val - other.m_val;
+    }
+
+    public IntValue increment()
+    {
+        return add(1);
+    }
+
+    public IntValue decrement()
+    {
+        return subtract(1);
+    }
+
+
+    public String toString()
+    {
+        return m_val + "";
+    }
 }
