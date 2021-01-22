a3a4bec6226de5e976308caa8529ae168ec485b5
gencdevops <67705077+gencdevops@users.noreply.github.com>
2021-01-22 22:22:17 +0300

    Date.getDaySuffix was added

--- src/org/csystem/util/datetime/Date.java
+++ src/org/csystem/util/datetime/Date.java
@@ -87,4 +87,28 @@ public class Date {
         m_dayOfWeek = getDayOfWeek(m_day, m_month, m_year);
     }
 
+    private String getDaySuffix()
+    {
+        String suffix;
+
+        switch (m_day) {
+            case 1:
+            case 21:
+            case 31:
+                suffix = "st";
+                break;
+            case 2:
+            case 22:
+                suffix = "nd";
+                break;
+            case 3:
+            case 23:
+                suffix = "rd";
+                break;
+            default:
+                suffix = "th";
+        }
+
+        return suffix;
+    }
 }
