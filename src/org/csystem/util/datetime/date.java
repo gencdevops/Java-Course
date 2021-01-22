2f8f2e2bc0ddccefb488849358ac6f98859a0b1b
gencdevops <67705077+gencdevops@users.noreply.github.com>
2021-01-22 22:16:29 +0300

    Date class initialize

---
+++ src/org/csystem/util/datetime/date.java
@@ -0,0 +1,20 @@
+package org.csystem.util.datetime;
+
+public class date {
+    private static final int [] DAYS_OF_MONTHS = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    private static final String [] DAYS_OF_WEEK_TR = {"Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"};
+    private static final String [] DAYS_OF_WEEK_EN = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+    private static final String [] MONTHS_TR = {"", "Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran",
+            "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"};
+    private static final String [] MONTHS_EN = {"", "Jan", "Feb", "Mar", "Apr", "May", "JUn", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+
+    private int m_day;
+    private int m_month;
+    private int m_year;
+    private int m_dayOfWeek;
+
+
+
+
+
+}
