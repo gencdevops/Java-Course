0ad017a9744b7ed26e2eaff7b0a29a85ec48dbe7
gencdevops <67705077+gencdevops@users.noreply.github.com>
2021-01-27 17:44:56 +0300

    CentralLimitTheoremSimulation initialize

---
+++ src/org/csystem/app/samples/centrallimit/CentralLimitTheoremSimulation.java
@@ -0,0 +1,17 @@
+package org.csystem.app.samples.centrallimit;
+
+import java.util.Random;
+
+public class CentralLimitTheoremSimulation {
+    private final int m_max;
+    private final int m_sampleCount;
+    private final int m_count;
+    private final int [] m_counts;
+    private final Random m_random;
+    private final int m_divisor;
+
+
+
+
+
+}
--- src/org/csystem/util/datetime/DayOfWeek.java
+++ src/org/csystem/util/datetime/DayOfWeek.java
@@ -3,3 +3,5 @@ package org.csystem.util.datetime;
 public enum DayOfWeek {
     SUN , MON , TUE , WED , THU , FRI , SAT
 }
+
+
