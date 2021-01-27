9d97cc299b3d3d3ea63268477fbba7506f7745a7
gencdevops <67705077+gencdevops@users.noreply.github.com>
2021-01-27 17:45:39 +0300

    CentralLimitTheoremSimulation.calculateSamplesAverage was added

--- src/org/csystem/app/samples/centrallimit/CentralLimitTheoremSimulation.java
+++ src/org/csystem/app/samples/centrallimit/CentralLimitTheoremSimulation.java
@@ -10,6 +10,16 @@ public class CentralLimitTheoremSimulation {
     private final Random m_random;
     private final int m_divisor;
 
+    private double calculateSamplesAverage()
+    {
+        int sum = 0;
+
+        for (int i = 0; i < m_sampleCount; ++i)
+            sum += m_random.nextInt(m_max + 1);
+
+        return (double)sum / m_sampleCount;
+    }
+
 
 
 
