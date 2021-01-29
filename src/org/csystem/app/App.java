54f4cb342fff1a82822017c809994de09723de2d
gencdevops <67705077+gencdevops@users.noreply.github.com>
2021-01-29 12:22:11 +0300

    Month enum initialize

--- src/org/csystem/app/App.java
+++ src/org/csystem/app/App.java
@@ -1,16 +1,34 @@
 package org.csystem.app;
 
 import org.csystem.util.ArrayUtil;
+import org.csystem.util.datetime.DayOfWeek;
 import org.csystem.util.math.geometry.Circle;
+
+import java.awt.*;
 
 class App {
 
     public static void main(String [] args) {
+        Color color = Color.RED;
 
-        System.out.println(Direction.LEFT.ordinal());
+        System.out.println(color.getB() + " " + color.getG() + " " + color.getR());
+
+
     }
 }
 
-enum Direction {
-    LEFT , TOP, BOTTOM, RİGHT
+enum Color {
+    RED(255, 0, 0), GREEN(0, 255, 0), BLUE(0, 0, 255), WHITE(255, 255, 255), BLACK(0, 0, 0);
+    private final int m_r, m_g, m_b;
+
+    Color(int r, int g, int b) {
+        m_r = r;
+        m_g = g;
+        m_b = b;
+    }
+
+    public int getR() {return m_r;}
+    public int getG() {return m_g;}
+    public int getB() {return m_b;}
+
 }
---
+++ src/org/csystem/util/datetime/Month.java
@@ -0,0 +1,14 @@
+package org.csystem.util.datetime;
+
+public enum Month {
+    JAN(31), FEB(28), MAR(31), APR(30), MAY(31), JUN(30), JUL(31), AUG(31), SEP(30), OCT(31), NOV(30), DEC(31);
+
+    private final int m_days;
+
+    Month(int days) {
+        m_days = days;
+    }
+
+
+
+}
