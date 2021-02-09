57fb45dafe7083aa0656552323e11a077603feda
gencdevops <67705077+gencdevops@users.noreply.github.com>
2021-02-09 16:35:59 +0300

    AnalyticalCircle constructor initialize

--- src/org/csystem/util/math/geometry/AnaliyticalCircle.java
+++ src/org/csystem/util/math/geometry/AnaliyticalCircle.java
@@ -3,6 +3,12 @@ package org.csystem.util.math.geometry;
 public class AnaliyticalCircle extends Circle  {
     private Point m_center;
 
+    public AnaliyticalCircle(double r, int x, int y) {
+        super(r);
+
+
+
+    }
 
 
 }
