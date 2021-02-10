e9bfa82f40d1a5a8c291893ac168bf1088799168
gencdevops <67705077+gencdevops@users.noreply.github.com>
2021-02-10 14:06:11 +0300

    Employee getter and setter methods added

--- src/org/csystem/app/samples/companyapp/Employee.java
+++ src/org/csystem/app/samples/companyapp/Employee.java
@@ -2,12 +2,24 @@ package org.csystem.app.samples.companyapp;
 
 public class Employee {
     private String m_citizenId;
-    private String name;
+    private String m_name;
 
     public Employee(String citizenId , String name) {
         this.m_citizenId = citizenId;
-        this.name = name;
+        this.m_name = name;
     }
 
+    public String getCitizenId() {
+        return m_citizenId;
+    }
+    public void setCitizenId(String citizenId) {
+        m_citizenId = citizenId;
+    }
+    public String getName() {
+        return m_name;
+    }
+    public void setName(String name) {
+        this.m_name = name;
+    }
 
 }
