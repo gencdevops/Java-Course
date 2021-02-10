9ebdf750ab6d640693b97e6b792f80e184ecdbd7
gencdevops <67705077+gencdevops@users.noreply.github.com>
2021-02-10 15:02:27 +0300

    CompanyApp test

--- src/org/csystem/app/samples/companyapp/CompanyApp.java
+++ src/org/csystem/app/samples/companyapp/CompanyApp.java
@@ -6,5 +6,12 @@ public class CompanyApp {
     }
     public void run() {
 
+        Worker worker = new Worker("12345678912", "Ali" , 100 , 8);
+        Manager manager = new Manager("12345678924" , "Veli" , 20_000 , "Software");
+
+        HumanResource humanResource = new HumanResource();
+        humanResource.payInsurance(worker);
+        humanResource.payInsurance(manager);
+
     }
 }
