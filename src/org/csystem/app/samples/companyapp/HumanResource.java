c14bf545d23845d88fc53367484635d138496f15
gencdevops <67705077+gencdevops@users.noreply.github.com>
2021-02-10 14:18:51 +0300

    Employee HumanResource.payInsurance method added

--- src/org/csystem/app/samples/companyapp/HumanResource.java
+++ src/org/csystem/app/samples/companyapp/HumanResource.java
@@ -1,5 +1,9 @@
 package org.csystem.app.samples.companyapp;
 
 public class HumanResource {
+    public void payInsurance(Employee employee) {
 
+        System.out.printf("Citizen ıd : %s" , employee.getCitizenId());
+        System.out.printf("Name : %s " , employee.getName());
+    }
 }
