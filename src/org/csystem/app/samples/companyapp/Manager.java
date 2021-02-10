2510aa61f415ac900071237adb8fd7f1bdba6141
gencdevops <67705077+gencdevops@users.noreply.github.com>
2021-02-10 14:50:46 +0300

    Manager class constructor and getter setter methods added

--- src/org/csystem/app/samples/companyapp/Manager.java
+++ src/org/csystem/app/samples/companyapp/Manager.java
@@ -2,7 +2,25 @@ package org.csystem.app.samples.companyapp;
 
 public class Manager extends Employee{
     private double m_salary;
-    private double m_department;
+    private String m_department;
 
+    public Manager(String citizenId , String name, double salary, String department) {
+        super(citizenId , name);
+        this.m_salary = salary;
+        this.m_department = department;
+    }
+
+    public double getSalary() {
+        return m_salary;
+    }
+    public void setSalary(double salary) {
+        this.m_salary = m_salary;
+    }
+    public String getDepartment() {
+        return m_department;
+    }
+    public void setDepartment(String department) {
+        this.m_department = department;
+    }
 
 }
