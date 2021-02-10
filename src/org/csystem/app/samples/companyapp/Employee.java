983ce7b2bb78cea06bb52ae8dbaa4df3e8877317
gencdevops <67705077+gencdevops@users.noreply.github.com>
2021-02-10 14:00:50 +0300

    Employee constructor added

--- src/org/csystem/app/samples/companyapp/Employee.java
+++ src/org/csystem/app/samples/companyapp/Employee.java
@@ -4,4 +4,10 @@ public class Employee {
     private String m_citizenId;
     private String name;
 
+    public Employee(String citizenId , String name) {
+        this.m_citizenId = citizenId;
+        this.name = name;
+    }
+
+
 }
