b8d9b82f639ac35c84c15bed9bf58fd5b719e97c
gencdevops <67705077+gencdevops@users.noreply.github.com>
2021-02-11 13:06:28 +0300

    RandomObjectArrayGenerator getObjects adde

--- src/org/csystem/app/samples/randomobjectgeneratorapp/RandomObjectArrayGenerator.java
+++ src/org/csystem/app/samples/randomobjectgeneratorapp/RandomObjectArrayGenerator.java
@@ -1,5 +1,22 @@
 package org.csystem.app.samples.randomobjectgeneratorapp;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class RandomObjectArrayGenerator {
+
+       private final Random m_random;
+       private final Object [] m_objects;
+
+       public RandomObjectArrayGenerator(int n) {
+           m_random = new Random();
+           m_objects = new Object[n];
+
+       }
+       public Object [] getObjects() {
+          return m_objects;
+       }
+
+
 
 }
