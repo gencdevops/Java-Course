18f126f542763c667682f62999d31a311a4c0fa3
gencdevops <67705077+gencdevops@users.noreply.github.com>
2021-02-26 13:36:11 +0300

    CSDArrayList doWorkForIllegalArgumentException methods was added

--- src/org/csystem/util/collection/CSDArrayList.java
+++ src/org/csystem/util/collection/CSDArrayList.java
@@ -1,5 +1,13 @@
 package org.csystem.util.collection;
 
 public class CSDArrayList {
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object [] m_elems;
+    private int m_index;
 
+    private static void doWorkForIllegalArgumentException(String message)
+    {
+        System.out.println(message);
+        System.exit(-1); //exception işlemleri konusuna kadar sabredin
+    }
 }
