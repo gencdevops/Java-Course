72664ce18b6362454925528e3ff429f7548284ba
gencdevops <67705077+gencdevops@users.noreply.github.com>
2021-02-26 13:38:32 +0300

    CSDArrayList trimToSize and remocve methods were added

--- src/org/csystem/util/collection/CSDArrayList.java
+++ src/org/csystem/util/collection/CSDArrayList.java
@@ -29,5 +29,102 @@ public class CSDArrayList {
             doWorkForIllegalArgumentException("Capacity can not be negative");
     }
 
+    private void changeCapacity(int capacity)
+    {
+        Object [] temp = new Object[capacity];
+
+        for (int i = 0; i < m_index; ++i)
+            temp[i] = m_elems[i];
+
+        m_elems = temp;
+    }
+
+    public CSDArrayList()
+    {
+        m_elems = new Object[DEFAULT_CAPACITY];
+    }
+
+    public CSDArrayList(int initialCapacity)
+    {
+        this.checkCapacityValue(initialCapacity);
+        m_elems = new Object[initialCapacity];
+    }
+
+    public boolean add(Object elem)
+    {
+        if (m_index == m_elems.length)
+            this.changeCapacity(m_elems.length == 0 ? 1 : m_elems.length * 2);
+
+        m_elems[m_index++] = elem;
+
+        return true;
+    }
+
+    public void add(int index, Object elem)
+    {
+        //...
+        if (m_index == m_elems.length)
+            this.changeCapacity(m_elems.length == 0 ? 1 : m_elems.length * 2);
+
+        //TODO:
+    }
+
+    public int capacity()
+    {
+        return m_elems.length;
+    }
+
+    public void clear()
+    {
+        for (int i = 0; i < m_index; ++i) //Dikkat bu yapılmazsa bellek sızıntısı (memory leak) oluşabilir
+            m_elems[i] = null;
+
+        m_index = 0;
+    }
+
+    public void ensureCapacity(int minCapacity)
+    {
+        if (minCapacity <= m_elems.length)
+            return;
+
+        this.changeCapacity(Math.max(minCapacity, m_elems.length * 2));
+    }
+
+    public Object get(int index)
+    {
+        this.checkForIndex(index);
+
+        return m_elems[index];
+    }
+
+    public Object remove(int index)
+    {
+        Object oldElem = m_elems[index];
+
+        //TODO:
+
+        return oldElem;
+    }
+
+    public Object set(int index, Object elem)
+    {
+        this.checkForIndex(index);
+        Object oldElem = m_elems[index];
+
+        m_elems[index] = elem;
+
+        return oldElem;
+    }
+
+    public int size()
+    {
+        return m_index;
+    }
+
+    public void trimToSize()
+    {
+        this.changeCapacity(m_index);
+    }
+
 
 }
