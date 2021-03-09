d50d0d7b6d06e4fefb18bb04f9d03152579e0634
gencdevops <67705077+gencdevops@users.noreply.github.com>
2021-03-09 14:49:21 +0300

    parserapp Source class initialize

--- src/org/csystem/app/App.java
+++ src/org/csystem/app/App.java
@@ -5,9 +5,7 @@ class App {
 
     public static void main(String [] args) {
 
-        System.out.println(10 + 20 + "istanbul"); //30istanbul
 
-        System.out.println("istanbul" + 10 + 20); //istanbul1020
 
 
     }
---
+++ src/org/csystem/app/samples/parserapp/Source.java
@@ -0,0 +1,5 @@
+package org.csystem.app.samples.parserapp;
+
+public class Source {
+
+}
